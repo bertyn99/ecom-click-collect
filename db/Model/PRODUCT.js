7a0e9e06999776997e71b8ccec55c88001f013f3
bertyn99 <bboulikou@gmail.com>
2021-05-08 23:44:03 +0200

    ajout  du sku pour l'inventaire

--- db/Model/PRODUCT.js
+++ db/Model/PRODUCT.js
@@ -12,6 +12,7 @@ const reviewSchema = new mongoose.Schema(
     }
 );
 const prodctSchema = new mongoose.Schema({
+    sku: { type: String, required: true },
     name: { type: String, required: true },
     image: { type: String, required: true },
     brand: { type: String, required: true },
@@ -19,8 +20,8 @@ const prodctSchema = new mongoose.Schema({
     category: { type: String, required: true },
     countInStock: { type: Number, default: 0, required: true },
     description: { type: String, required: true },
-    rating: { type: Number, default: 0, required: true },
-    numReviews: { type: Number, default: 0, required: true },
+    /*  rating: { type: Number, default: 0, required: true }, 
+        numReviews: { type: Number, default: 0, required: true }, */
     reviews: [reviewSchema],
 });
 
