b8288b0b1d7982dc7d20ed20d337af1749b114cb
bertyn <bertynboulikou@gmail.com>
2021-06-04 17:31:58 +0200

    modif config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const config = {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 3200,
     DBURL: process.env.DBURL,
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
 };
