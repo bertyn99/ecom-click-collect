68206280ec861d9eb2b19f7326274d0bcaddcc12
bertyn <bertynboulikou@gmail.com>
2021-06-05 17:30:20 +0200

    ajout de smethode dans admin controller

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,2 +1,29 @@
-const Admin = require("../db/model/USER");
+const Staff = require("../db/model/staff");
 const database = require("../db/connexion");
+
+
+async function getAllStaff(req, res) {
+    try {
+
+        const all = await Staff.find()
+        const allStaff = all.map(({ password, ...rest }) => rest)
+        res.status(200).send(allStaff)
+    } catch (e) {
+        res.status(404).send("Cant get all the Staff")
+    }
+}
+
+async function updaterole() {
+    try {
+
+    }
+    catch (e) {
+        res.status(404).send("Cant get all the Staff")
+    }
+}
+
+
+module.exports = {
+    getAllStaff,
+
+}
