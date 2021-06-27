63ecdd5a4dbe725804fba95abf609771934f7afd
bertyn99 <bboulikou@gmail.com>
2021-06-27 18:22:22 +0200

    ajout de nouvelle variable d'env

--- config.js
+++ config.js
@@ -5,5 +5,8 @@ const config = {
     PORT: process.env.PORT || 3200,
     DBURL: process.env.DBURL,
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+    CLOUD_NAME: process.env.CLOUD_NAME,
+    API_KEY: process.env.API_KEY,
+    API_SECRET_KEY: process.env.API_KEY
 };
 module.exports = config
