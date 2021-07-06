9eb2fd4a6e4dc4f011dd9773671039b7755943d5
bertyn <bertynboulikou@gmail.com>
2021-07-06 11:01:30 +0200

    ajout description

--- db/Model/ARTICLE.js
+++ db/Model/ARTICLE.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const articleSchema = new mongoose.Schema({
     author: String,
     cover: [{ type: String }],
+    desc: { type: String, required: true },
     content: String,
     isPublished: Boolean,
     publishingDate: Date
