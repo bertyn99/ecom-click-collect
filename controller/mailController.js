6c4ef1fd6b6029ab92c4a154c23220eb26a8c16c
bertyn <bertynboulikou@gmail.com>
2021-07-13 13:51:14 +0200

    fix bug mail

--- controller/mailController.js
+++ controller/mailController.js
@@ -5,7 +5,7 @@ function SendContactMail(req, res) {
 
         const mailData = {
             from: "website@hawabowls.com",
-            to: "comunication.hawabowls@gmail.com",
+            to: "communication.hawabowls@gmail.com",
             subject: "Formulaire de contact du site web",
             html: `<p>&nbsp; &nbsp; Bonjour,<br><br>&nbsp;Vous&nbsp;avez&nbsp;reçu&nbsp;un&nbsp;nouveau&nbsp;messagede&nbsp;la&nbsp;part&nbsp;de : <strong>${req.body.firstname + ' ' + req.body.lastname}</strong><br><br>Il représente :<em><strong>${req.body.entreprise}</strong></em><br><br>Comment a-t-il entendu parlé de nous?: ${req.body.aboutus}<br><br>Voici son message :<br>   ${req.body.content} <br><br>Voici ces contact:<br>&nbsp; &nbsp;-Email: <a target="_blank" href="mailto:${req.body.email}">${req.body.email}</a><br>&nbsp; &nbsp;-Tel: &nbsp ${req.body.mobile};<br><br><br></p>`
 
